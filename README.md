ae865e0f64886284cd4e821f2d626ae90aa906bc
Tomas Bracalenti <tobracalenti@frsf.utn.edu.ar>
2024-05-07 21:51:33 -0300

    Hasta Ej 1.5

---
+++ README.md
@@ -0,0 +1,1 @@
+# Bracalenti-bootcamp
---
+++ part1/courseInfo/src/components/Total.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Total = (props) => {
+    const exercises1 = props.parts[0].exercises;
+    const exercises2 = props.parts[1].exercises;
+    const exercises3 = props.parts[2].exercises;
+    return (
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    );
+}
+export default Total;
