e450f897ccc9d60c076abccf01d60fb079d8e7a5
Tomas Bracalenti <tobracalenti@frsf.utn.edu.ar>
2024-06-11 21:57:43 -0300

    Ejercicos 3.15 a 3.18

---
+++ part3/README.md
@@ -0,0 +1,1 @@
+https://bracalenti-bootcamp.onrender.com/
--- part3/index.js
+++ part3/index.js
@@ -1,77 +1,59 @@
-const express = require("express");
+require("dotenv").config();
 
+
+const express = require("express");
 const app = express();
 app.use(express.json());
-const morgan = require("morgan");
 
+
+const morgan = require("morgan");
 app.use(morgan("tiny"));
 morgan.token("body", (req, res) => JSON.stringify(req.body));
-
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
+
 
 const cors = require("cors");
 app.use(cors());
 
-app.use(express.static("build"));
+const Person = require("./models/person");
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
+app.use(express.static("build"));
 
 app.get("/", (req, res) => {
     res.send("<h1>Backend PhoneBook</h1>");
 });
 
 app.get("/api/persons", (req, res) => {
-    res.send(persons);
+    Person.find({}).then(persons => {
+        res.json(persons);
+    });
 });
 
-app.get("/info", (req, res) => {
-    res.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        <p>${new Date()}</p>`
-    );
+app.get("/info", (req, res, next) => {
+    Person.find({}).then(persons => {
+        res.send(
+            `<p>Phonebook has info for ${persons.length} people</p>
+            <p>${new Date()}</p>`
+        );
+    })
+    .catch(error => next(error))
+    ;
 });
-app.get("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const person = persons.find((person) => person.id === id);
-    if(person) {
+app.get("/api/persons/:id", (req, res, next) => {
+    Person.findById(req.params.id).then(person => {
         res.json(person);
-    } else {
-        res.status(404).end();
-    }
+    })
+    .catch(error => next(error))
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    console.log(req.params.name);
-    persons = persons.filter((person) => person.id !== id);
-    res.status(204).end();
+    Person.findByIdAndDelete(req.params.id).then(() => {
+        res.status(204).end();
+    })
+    .catch(error => next(error))
 });
 
-const generateId = () => {
-    const id = Math.floor(Math.random() * 1000000);
-    return id;
-}
+
 
 app.post("/api/persons", (req, res) => {
     const body = req.body;
@@ -80,22 +62,38 @@ app.post("/api/persons", (req, res) => {
             error: "name or number is missing"
         });
     }
-    if(persons.find(person => person.name===body.name)) {
-        return res.status(400).json({
-            error: "name must be unique"
-        });
-    }
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateId(),
-    };
-    persons = persons.concat(person);
-    res.json(person);
+    });
+    person.save().then(savedPerson => {
+        res.json(savedPerson);
+    });
+
 });
 
 
-const PORT = process.env.PORT || 3001;
+// Manejador de errores de solicitudes desconocidas (debe ser el último middleware)
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({error: "unknown endpoint"});
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message);
+    if(error.name === "CastError") {
+        return res.status(400).send({error: "malformatted id"});
+    }
+    next(error);
+};
+
+app.use(errorHandler);
+
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    
+    //console.log(`http://localhost:${PORT}`);
 });
